84797567b2b5c57eb2da1e7f0106a219db2cd624
dmytrobaida <dmytrobaida@outlook.com>
2024-02-24 01:56:12 +0200

    adds export

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -4,8 +4,9 @@ import 'tailwindcss/tailwind.css';
 import Header from './lib/components/Header';
 import Footer from './lib/components/Footer';
 import Content from './lib/components/Content';
+import Wrapper from './lib/components/Wrapper';
 import PortfolioMdProvider from './lib/components/PortfolioMdProvider';
 
 export * from '@nextui-org/react';
 export { useTheme } from 'next-themes';
-export { Header, Footer, Content, PortfolioMdProvider };
+export { Header, Footer, Content, Wrapper, PortfolioMdProvider };
